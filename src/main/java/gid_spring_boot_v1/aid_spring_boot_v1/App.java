99944f4771aef12f759309c207b301c1762dd4ac
Carlos-PC\Carlos <carlosaraujogonzalez@gmail.com>
2019-07-10 00:13:16 +0200

    finished project

---
+++ src/main/java/gid_spring_boot_v1/aid_spring_boot_v1/App.java
@@ -0,0 +1,16 @@
+package gid_spring_boot_v1.aid_spring_boot_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main App
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args ){
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/gid_spring_boot_v1/aid_spring_boot_v1/GreetingController.java
@@ -0,0 +1,18 @@
+package gid_spring_boot_v1.aid_spring_boot_v1;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    //The @RequestMapping annotation ensures that HTTP requests to /greeting are mapped to the greeting() method.
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
